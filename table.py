2b3fabc5a2d7b28c4770e550fe0be3e0788febc1
Nils Martel <nilsmartel@yahoo.de>
2022-09-26 13:44:00 +0200

    dbg

--- table.py
+++ table.py
@@ -1,12 +1,6 @@
 class TableMatrix:
 
     def set(self, row, col, value):
-        if isinstance(col, str):
-            col = self.cols.index(col)
-
-        if isinstance(row, str):
-            row = self.rows.index(row)
-
         i = self.__get_data_index(row, col)
         self.data[i] = value
 
@@ -16,6 +10,12 @@ class TableMatrix:
 
 
     def __get_data_index(self, row, col):
+        if isinstance(col, str):
+            col = self.cols.index(col)
+
+        if isinstance(row, str):
+            row = self.rows.index(row)
+
         assert row < self.height
         assert col < self.width
 
